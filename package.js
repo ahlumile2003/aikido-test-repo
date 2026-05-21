c5ed69fb45e7bb66a3e46e27349770b07b946978
ahlumile2003 <152970251+ahlumile2003@users.noreply.github.com>
2026-05-21 09:25:12 +0200

    Create package.json for Node.js project setup
    Initialized package.json with project details and dependencies.

---
+++ package.js
@@ -0,0 +1,22 @@
+{
+  "name": "my-cool-app",
+  "version": "1.0.0",
+  "description": "A simple example Node.js project",
+  "main": "index.js",
+  "type": "module",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js",
+    "test": "jest"
+  },
+  "keywords": ["node", "example", "javascript"],
+  "author": "Your Name <email@example.com>",
+  "license": "MIT",
+  "dependencies": {
+    "express": "^4.18.2"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1",
+    "jest": "^29.6.0"
+  }
+}
